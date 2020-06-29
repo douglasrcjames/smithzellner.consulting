01c47d3d715d6d1b2fa7c807cfa76f2ef162c035
Douglas James <douglasrcjames@gmail.com>
2020-06-29 13:06:53 -0700

    Initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div className="wrapper">
+                <h1>Home</h1>
+                {/* TODO: update content */}
+                <p>Welcome to Doug's React Boiler! Click around a bit, it's a clean slate for you!</p>
+            </div>
+        )
+    }
+}
